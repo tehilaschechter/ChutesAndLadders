fc6ed05ae1ac75376acd2cd355c81c48b631ed5e
Tehila <tehilaschechter@gmail.com>
2020-10-25 21:15:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/chutesandladders/Box.java
@@ -0,0 +1,57 @@
+package com.example.chutesandladders;
+
+public class Box {
+    private int boxNumber;
+    private boolean snakeBeginning;
+    private boolean ladderBeginning;
+    private boolean isLast;
+    private boolean isFreeTurn;
+
+    public Box(int boxNumber, boolean snakeBeginning, boolean ladderBeginning, boolean isLast, boolean isFreeTurn) {
+        this.boxNumber = boxNumber;
+        this.snakeBeginning = snakeBeginning;
+        this.ladderBeginning = ladderBeginning;
+        this.isLast = isLast;
+        this.isFreeTurn = isFreeTurn;
+    }
+
+    public int getBoxNumber() {
+        return boxNumber;
+    }
+
+    public void setBoxNumber(int boxNumber) {
+        this.boxNumber = boxNumber;
+    }
+
+    public boolean isSnakeBeginning() {
+        return snakeBeginning;
+    }
+
+    public void setSnakeBeginning(boolean snakeBeginning) {
+        this.snakeBeginning = snakeBeginning;
+    }
+
+    public boolean isLadderBeginning() {
+        return ladderBeginning;
+    }
+
+    public void setLadderBeginning(boolean ladderBeginning) {
+        this.ladderBeginning = ladderBeginning;
+    }
+
+    public boolean isLast() {
+        return isLast;
+    }
+
+    public void setLast(boolean last) {
+        isLast = last;
+    }
+
+    public boolean isFreeTurn() {
+        return isFreeTurn;
+    }
+
+    public void setFreeTurn(boolean freeTurn) {
+        isFreeTurn = freeTurn;
+    }
+}
---
+++ app/src/main/java/com/example/chutesandladders/Player.java
@@ -0,0 +1,27 @@
+package com.example.chutesandladders;
+
+public class Player {
+    private int playerID;
+    private int currentBoxNumber;
+
+    public Player(int playerID, int currentBoxNumber) {
+        this.playerID = playerID;
+        this.currentBoxNumber = currentBoxNumber;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public int getCurrentBoxNumber() {
+        return currentBoxNumber;
+    }
+
+    public void setCurrentBoxNumber(int currentBoxNumber) {
+        this.currentBoxNumber = currentBoxNumber;
+    }
+}
